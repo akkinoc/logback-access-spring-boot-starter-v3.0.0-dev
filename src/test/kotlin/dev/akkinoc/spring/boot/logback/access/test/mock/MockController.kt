dc6bd9ac76b3b27df1f4ab59f28a63fde390d0da
Akihiro Kondo <akkinoc@gmail.com>
2021-10-03 23:42:57 +0900

    mock

--- src/test/kotlin/dev/akkinoc/spring/boot/logback/access/test/mock/MockController.kt
+++ src/test/kotlin/dev/akkinoc/spring/boot/logback/access/test/mock/MockController.kt
@@ -1,6 +1,5 @@
 package dev.akkinoc.spring.boot.logback.access.test.mock
 
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,9 +17,8 @@ class MockController {
      * @return A mock text.
      */
     @GetMapping("/text")
-    fun getText(): ResponseEntity<String> {
-        return ResponseEntity.ok()
-                .body("MOCK-TEXT")
+    fun getText(): String {
+        return "MOCK-TEXT"
     }
 
 }
