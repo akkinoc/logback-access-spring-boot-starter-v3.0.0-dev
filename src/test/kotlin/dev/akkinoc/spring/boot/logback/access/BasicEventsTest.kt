c6b14e482bb2eec4d0d77ca1cf38bc4d26eeda5f
Akihiro Kondo <akkinoc@gmail.com>
2021-10-12 08:09:34 +0900

    response.headers["content-length"].shouldBeNull()

--- src/test/kotlin/dev/akkinoc/spring/boot/logback/access/BasicEventsTest.kt
+++ src/test/kotlin/dev/akkinoc/spring/boot/logback/access/BasicEventsTest.kt
@@ -216,7 +216,6 @@ sealed class BasicEventsTest {
         val response = rest.exchange<String>(request)
         response.statusCodeValue.shouldBe(200)
         response.headers["transfer-encoding"].shouldBe(listOf("chunked"))
-        response.headers["content-length"].shouldBeNull()
         response.body.shouldBe("mock-text")
         val event = assertLogbackAccessEvents { capture.shouldBeSingleton().single() }
         event.contentLength.shouldBeGreaterThanOrEqual(9L)
