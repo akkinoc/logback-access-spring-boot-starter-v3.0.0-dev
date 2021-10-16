c520eabdf19fa7c73a04bc7ce8c9be8a861fc191
Akihiro Kondo <akkinoc@gmail.com>
2021-10-16 15:19:23 +0900

    fix-kdoc

--- src/test/kotlin/dev/akkinoc/spring/boot/logback/access/TeeFilterActiveTest.kt
+++ src/test/kotlin/dev/akkinoc/spring/boot/logback/access/TeeFilterActiveTest.kt
@@ -145,7 +145,7 @@ class UndertowReactiveWebTeeFilterHostIncludedTest : TeeFilterHostIncludedTest(
 )
 
 /**
- * Tests the case where the current host is included in the tee filter's exclusion host list.
+ * Tests the case where the current host is not included in the tee filter's exclusion host list.
  *
  * @property supportsRequestContents Whether to support request contents.
  * @property supportsResponseContents Whether to support response contents.
