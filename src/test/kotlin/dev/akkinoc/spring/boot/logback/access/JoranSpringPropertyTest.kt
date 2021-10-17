d2b293dba1baf718ee35aad1e28e2257089b5dd2
Akihiro Kondo <akkinoc@gmail.com>
2021-10-17 15:43:15 +0900

    JoranSpringPropertyTest

---
+++ src/test/kotlin/dev/akkinoc/spring/boot/logback/access/JoranSpringPropertyTest.kt
@@ -0,0 +1,84 @@
+package dev.akkinoc.spring.boot.logback.access
+
+import dev.akkinoc.spring.boot.logback.access.test.assertion.Assertions.assertLogbackAccessEventsEventually
+import dev.akkinoc.spring.boot.logback.access.test.type.JettyReactiveWebTest
+import dev.akkinoc.spring.boot.logback.access.test.type.JettyServletWebTest
+import dev.akkinoc.spring.boot.logback.access.test.type.TomcatReactiveWebTest
+import dev.akkinoc.spring.boot.logback.access.test.type.TomcatServletWebTest
+import dev.akkinoc.spring.boot.logback.access.test.type.UndertowReactiveWebTest
+import dev.akkinoc.spring.boot.logback.access.test.type.UndertowServletWebTest
+import io.kotest.matchers.collections.shouldHaveSingleElement
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.system.CapturedOutput
+import org.springframework.boot.test.system.OutputCaptureExtension
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
+import org.springframework.http.RequestEntity
+import org.springframework.test.context.TestPropertySource
+
+/**
+ * Tests the case where using `<springProperty>` tags in the configuration file.
+ */
+@ExtendWith(OutputCaptureExtension::class)
+@TestPropertySource(
+        properties = [
+            "logback.access.config=classpath:logback-access-test-spring.property.xml",
+            "logback.access.test.console.pattern.prefix=>>>",
+            "logback.access.test.console.pattern.suffix=<<<",
+        ],
+)
+sealed class JoranSpringPropertyTest {
+
+    @Test
+    fun `Appends a Logback-access event`(
+            @Autowired rest: TestRestTemplate,
+            capture: CapturedOutput,
+    ) {
+        val request = RequestEntity.get("/mock-controller/text").build()
+        val response = rest.exchange<String>(request)
+        response.statusCodeValue.shouldBe(200)
+        response.body.shouldBe("mock-text")
+        val regex = Regex("""^>>>127\.0\.0\.1 - - \[.+] "GET /mock-controller/text HTTP/1\.1" 200 9<<<$""")
+        assertLogbackAccessEventsEventually { capture.out.lines().shouldHaveSingleElement { it matches regex } }
+    }
+
+}
+
+/**
+ * Tests the [JoranSpringPropertyTest] using the Tomcat servlet web server.
+ */
+@TomcatServletWebTest
+class TomcatServletWebJoranSpringPropertyTest : JoranSpringPropertyTest()
+
+/**
+ * Tests the [JoranSpringPropertyTest] using the Tomcat reactive web server.
+ */
+@TomcatReactiveWebTest
+class TomcatReactiveWebJoranSpringPropertyTest : JoranSpringPropertyTest()
+
+/**
+ * Tests the [JoranSpringPropertyTest] using the Jetty servlet web server.
+ */
+@JettyServletWebTest
+class JettyServletWebJoranSpringPropertyTest : JoranSpringPropertyTest()
+
+/**
+ * Tests the [JoranSpringPropertyTest] using the Jetty reactive web server.
+ */
+@JettyReactiveWebTest
+class JettyReactiveWebJoranSpringPropertyTest : JoranSpringPropertyTest()
+
+/**
+ * Tests the [JoranSpringPropertyTest] using the Undertow servlet web server.
+ */
+@UndertowServletWebTest
+class UndertowServletWebJoranSpringPropertyTest : JoranSpringPropertyTest()
+
+/**
+ * Tests the [JoranSpringPropertyTest] using the Undertow reactive web server.
+ */
+@UndertowReactiveWebTest
+class UndertowReactiveWebJoranSpringPropertyTest : JoranSpringPropertyTest()
