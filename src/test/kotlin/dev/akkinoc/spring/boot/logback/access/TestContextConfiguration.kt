a2b3cc9251dd6a54982bf26432d8552eeca1894f
Akihiro Kondo <akkinoc@gmail.com>
2021-10-21 19:46:16 +0900

    exclude-security-auto-config

--- src/test/kotlin/dev/akkinoc/spring/boot/logback/access/TestContextConfiguration.kt
+++ src/test/kotlin/dev/akkinoc/spring/boot/logback/access/TestContextConfiguration.kt
@@ -2,10 +2,12 @@ package dev.akkinoc.spring.boot.logback.access
 
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 
 /**
  * The test context configuration.
  */
 @SpringBootConfiguration(proxyBeanMethods = false)
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = [SecurityAutoConfiguration::class, ReactiveSecurityAutoConfiguration::class])
 class TestContextConfiguration
