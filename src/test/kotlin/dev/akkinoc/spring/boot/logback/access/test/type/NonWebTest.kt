e16bb36ebdf786bb272a212ffd3e37755ca2d831
Akihiro Kondo <akkinoc@gmail.com>
2021-10-24 01:27:03 +0900

    annotation-import

--- src/test/kotlin/dev/akkinoc/spring/boot/logback/access/test/type/NonWebTest.kt
+++ src/test/kotlin/dev/akkinoc/spring/boot/logback/access/test/type/NonWebTest.kt
@@ -1,15 +1,13 @@
 package dev.akkinoc.spring.boot.logback.access.test.type
 
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE
-import kotlin.annotation.AnnotationRetention.RUNTIME
-import kotlin.annotation.AnnotationTarget.CLASS
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 
 /**
  * Indicates a Spring Boot based test that does not use a web server.
  */
-@Target(CLASS)
-@Retention(RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
-@SpringBootTest(webEnvironment = NONE)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 annotation class NonWebTest
